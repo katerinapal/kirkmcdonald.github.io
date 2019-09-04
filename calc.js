88b6814a8e89cdaddbca0a73a8dc123ed1766c5f
katerina <katpaltoglou@gmail.com>
2019-09-04 16:27:35 +0300

    update transpiled branch

---
+++ calc.js
@@ -0,0 +1,210 @@
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-66808523-1', 'auto');
+  ga('send', 'pageview');
+
+var attributes = document.getElementsByTagName('body');
+for(var i = 0; i < attributes.length; i++) {
+attributes[i].onload = function() {
+	init();
+}
+}
+document.getElementById('+').onclick = function() {
+	plusHandler();
+}
+document.getElementById('totals_button').onclick = function() {
+	clickTab('totals_tab');
+}
+document.getElementById('graph_button').onclick = function() {
+	clickVisualize('graph_tab');
+}
+document.getElementById('settings_button').onclick = function() {
+	clickTab('settings_tab');
+}
+document.getElementById('faq_button').onclick = function() {
+	clickTab('faq_tab');
+}
+document.getElementById('about_button').onclick = function() {
+	clickTab('about_tab');
+}
+document.getElementById('debug_button').onclick = function() {
+	clickTab('debug_tab');
+}
+document.getElementById('CSV').onclick = function() {
+	toggleVisible('csv_box');
+}
+var attributes = document.getElementsByTagName('span');
+for(var i = 0; i < attributes.length; i++) {
+attributes[i].onclick = function() {
+	toggleVisualizerSettings();
+}
+}
+document.getElementById('vis_sankey').onchange = function() {
+	changeVisualizerType(event);
+}
+document.getElementById('vis_box').onchange = function() {
+	changeVisualizerType(event);
+}
+document.getElementById('visdir_right').onchange = function() {
+	changeVisualizerDirection(event);
+}
+document.getElementById('visdir_down').onchange = function() {
+	changeVisualizerDirection(event);
+}
+document.getElementById('vis-node-breadth').onchange = function() {
+	changeNodeBreadth(event);
+}
+document.getElementById('vis-link-length').onchange = function() {
+	changeLinkLength(event);
+}
+document.getElementById('data_set').onchange = function() {
+	changeMod();
+}
+document.getElementById('color_scheme').onchange = function() {
+	changeColor(event);
+}
+document.getElementById('rprec').onchange = function() {
+	changeRPrec(event);
+}
+document.getElementById('fprec').onchange = function() {
+	changeFPrec(event);
+}
+document.getElementById('kovarex').onchange = function() {
+	changeKovarex(event);
+}
+document.getElementById('pipe_length').onchange = function() {
+	changePipeLength(event);
+}
+document.getElementById('mprod').onchange = function() {
+	changeMprod();
+}
+document.getElementById('default_beacon_count').onchange = function() {
+	changeDefaultBeaconCount(event);
+}
+document.getElementById('topo_order').onchange = function() {
+	changeSortOrder(event);
+}
+document.getElementById('alpha_order').onchange = function() {
+	changeSortOrder(event);
+}
+document.getElementById('decimal_format').onchange = function() {
+	changeFormat(event);
+}
+document.getElementById('rational_format').onchange = function() {
+	changeFormat(event);
+}
+document.getElementById('tooltip').onchange = function() {
+	changeTooltip(event);
+}
+document.getElementById('render_debug').onchange = function() {
+	toggleDebug(event);
+}
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-66808523-1', 'auto');
+  ga('send', 'pageview');
+
+var attributes = document.getElementsByTagName('body');
+for(var i = 0; i < attributes.length; i++) {
+attributes[i].onload = function() {
+	init();
+}
+}
+document.getElementById('+').onclick = function() {
+	plusHandler();
+}
+document.getElementById('totals_button').onclick = function() {
+	clickTab('totals_tab');
+}
+document.getElementById('graph_button').onclick = function() {
+	clickVisualize('graph_tab');
+}
+document.getElementById('settings_button').onclick = function() {
+	clickTab('settings_tab');
+}
+document.getElementById('faq_button').onclick = function() {
+	clickTab('faq_tab');
+}
+document.getElementById('about_button').onclick = function() {
+	clickTab('about_tab');
+}
+document.getElementById('debug_button').onclick = function() {
+	clickTab('debug_tab');
+}
+document.getElementById('CSV').onclick = function() {
+	toggleVisible('csv_box');
+}
+var attributes = document.getElementsByTagName('span');
+for(var i = 0; i < attributes.length; i++) {
+attributes[i].onclick = function() {
+	toggleVisualizerSettings();
+}
+}
+document.getElementById('vis_sankey').onchange = function() {
+	changeVisualizerType(event);
+}
+document.getElementById('vis_box').onchange = function() {
+	changeVisualizerType(event);
+}
+document.getElementById('visdir_right').onchange = function() {
+	changeVisualizerDirection(event);
+}
+document.getElementById('visdir_down').onchange = function() {
+	changeVisualizerDirection(event);
+}
+document.getElementById('vis-node-breadth').onchange = function() {
+	changeNodeBreadth(event);
+}
+document.getElementById('vis-link-length').onchange = function() {
+	changeLinkLength(event);
+}
+document.getElementById('data_set').onchange = function() {
+	changeMod();
+}
+document.getElementById('color_scheme').onchange = function() {
+	changeColor(event);
+}
+document.getElementById('rprec').onchange = function() {
+	changeRPrec(event);
+}
+document.getElementById('fprec').onchange = function() {
+	changeFPrec(event);
+}
+document.getElementById('kovarex').onchange = function() {
+	changeKovarex(event);
+}
+document.getElementById('pipe_length').onchange = function() {
+	changePipeLength(event);
+}
+document.getElementById('mprod').onchange = function() {
+	changeMprod();
+}
+document.getElementById('default_beacon_count').onchange = function() {
+	changeDefaultBeaconCount(event);
+}
+document.getElementById('topo_order').onchange = function() {
+	changeSortOrder(event);
+}
+document.getElementById('alpha_order').onchange = function() {
+	changeSortOrder(event);
+}
+document.getElementById('decimal_format').onchange = function() {
+	changeFormat(event);
+}
+document.getElementById('rational_format').onchange = function() {
+	changeFormat(event);
+}
+document.getElementById('tooltip').onchange = function() {
+	changeTooltip(event);
+}
+document.getElementById('render_debug').onchange = function() {
+	toggleDebug(event);
+}
---
+++ calculation.js
@@ -0,0 +1,302 @@
+
+functionPlot({
+    target: "#solution",
+    xAxis: {
+        label: 'crude oil',
+        domain: [0, 120]
+    },
+    yAxis: {
+        label: 'recipe ratios',
+        domain: [0, 3]
+    },
+    annotations: [{
+        x: 2300/39,
+        text: 'minimum'
+    }, {
+        x: 200/3,
+        text: 'maximum'
+    }],
+    data: [{
+        fn: '13x/400 - 23/12',
+        color: 'red'
+    }, {
+        fn: '7x/400 - 1/4',
+        color: 'orange'
+    }, {
+        fn: '3x/50 - 10/3',
+        color: '#4040e8'
+    }, {
+        fn: '-x/20 + 10/3',
+        color: '#05b378'
+    }, {
+        fn: '-x + 305/3',
+        color: 'steelblue'
+    }]
+})
+
+var minimum = {
+    fn: 'x',
+    skipTip: true,
+    color: "red",
+}
+var options = {
+    target: "#vector",
+    xAxis: {
+        domain: [0, 100],
+        label: 'light oil',
+    },
+    yAxis: {
+        domain: [0, 100],
+        label: 'heavy oil',
+    },
+    disableZoom: true,
+    data: [minimum],
+}
+var vectors = functionPlot(options)
+function vectormousemove(coords) {
+    var x = coords.x
+    var y = coords.y
+    var recipes = [
+        [ 30, -40, "red"],
+        [ 30,  30, "#4040e8"],
+        [ 45,  10, "#05b378"],
+    ]
+    var solutions = [
+        [              0, -x/105 + 3*y/70,    x/35 - y/35],
+        [x/210 - 3*y/140,               0, 2*x/105 + y/70],
+        [    x/70 - y/70,  2*x/105 + y/70,              0]
+    ]
+    var first = [-1, 0]
+    var firstColor = "black"
+    var second = [0, 0]
+    var secondColor = "black"
+    for (var i in solutions) {
+        var valid = true
+        for (var j in solutions[i]) {
+            if (solutions[i][j] < 0) {
+                valid = false
+                break
+            }
+        }
+        if (valid) {
+            document.getElementById("crack").textContent = solutions[i][0]
+            document.getElementById("basic").textContent = solutions[i][1]
+            document.getElementById("adv").textContent = solutions[i][2]
+            var j = 0
+            var doFirst = true
+            for (j in solutions[i]) {
+                if (j == i) {
+                    continue
+                }
+                var recipe = recipes[j]
+                var current = [
+                    recipe[0] * solutions[i][j],
+                    recipe[1] * solutions[i][j],
+                ]
+                var color = recipe[2]
+                if (doFirst) {
+                    first = current
+                    firstColor = color
+                    doFirst = false
+                } else {
+                    second = current
+                    secondColor = color
+                }
+            }
+            break
+        }
+    }
+    options.data = [minimum]
+    if (first[0] == -1) {
+        document.getElementById("crack").textContent = "X"
+        document.getElementById("basic").textContent = "X"
+        document.getElementById("adv").textContent = "X"
+        options.data.push({
+            points: [
+                [30, 20],
+                [70, 80],
+            ],
+            fnType: 'points',
+            graphType: 'polyline',
+            color: 'red'
+        })
+        options.data.push({
+            points: [
+                [30, 80],
+                [70, 20],
+            ],
+            fnType: 'points',
+            graphType: 'polyline',
+            color: 'red'
+        })
+    } else {
+        options.data.push({
+            vector: second,
+            graphType: 'polyline',
+            fnType: 'vector',
+            skipTip: true,
+            color: secondColor,
+        })
+        options.data.push({
+            vector: first,
+            offset: second,
+            graphType: 'polyline',
+            fnType: 'vector',
+            skipTip: true,
+            color: firstColor,
+        })
+    }
+    vectors = functionPlot(options)
+}
+vectors.on("mousemove", vectormousemove)
+
+functionPlot({
+    target: "#solution",
+    xAxis: {
+        label: 'crude oil',
+        domain: [0, 120]
+    },
+    yAxis: {
+        label: 'recipe ratios',
+        domain: [0, 3]
+    },
+    annotations: [{
+        x: 2300/39,
+        text: 'minimum'
+    }, {
+        x: 200/3,
+        text: 'maximum'
+    }],
+    data: [{
+        fn: '13x/400 - 23/12',
+        color: 'red'
+    }, {
+        fn: '7x/400 - 1/4',
+        color: 'orange'
+    }, {
+        fn: '3x/50 - 10/3',
+        color: '#4040e8'
+    }, {
+        fn: '-x/20 + 10/3',
+        color: '#05b378'
+    }, {
+        fn: '-x + 305/3',
+        color: 'steelblue'
+    }]
+})
+
+var minimum = {
+    fn: 'x',
+    skipTip: true,
+    color: "red",
+}
+var options = {
+    target: "#vector",
+    xAxis: {
+        domain: [0, 100],
+        label: 'light oil',
+    },
+    yAxis: {
+        domain: [0, 100],
+        label: 'heavy oil',
+    },
+    disableZoom: true,
+    data: [minimum],
+}
+var vectors = functionPlot(options)
+function vectormousemove(coords) {
+    var x = coords.x
+    var y = coords.y
+    var recipes = [
+        [ 30, -40, "red"],
+        [ 30,  30, "#4040e8"],
+        [ 45,  10, "#05b378"],
+    ]
+    var solutions = [
+        [              0, -x/105 + 3*y/70,    x/35 - y/35],
+        [x/210 - 3*y/140,               0, 2*x/105 + y/70],
+        [    x/70 - y/70,  2*x/105 + y/70,              0]
+    ]
+    var first = [-1, 0]
+    var firstColor = "black"
+    var second = [0, 0]
+    var secondColor = "black"
+    for (var i in solutions) {
+        var valid = true
+        for (var j in solutions[i]) {
+            if (solutions[i][j] < 0) {
+                valid = false
+                break
+            }
+        }
+        if (valid) {
+            document.getElementById("crack").textContent = solutions[i][0]
+            document.getElementById("basic").textContent = solutions[i][1]
+            document.getElementById("adv").textContent = solutions[i][2]
+            var j = 0
+            var doFirst = true
+            for (j in solutions[i]) {
+                if (j == i) {
+                    continue
+                }
+                var recipe = recipes[j]
+                var current = [
+                    recipe[0] * solutions[i][j],
+                    recipe[1] * solutions[i][j],
+                ]
+                var color = recipe[2]
+                if (doFirst) {
+                    first = current
+                    firstColor = color
+                    doFirst = false
+                } else {
+                    second = current
+                    secondColor = color
+                }
+            }
+            break
+        }
+    }
+    options.data = [minimum]
+    if (first[0] == -1) {
+        document.getElementById("crack").textContent = "X"
+        document.getElementById("basic").textContent = "X"
+        document.getElementById("adv").textContent = "X"
+        options.data.push({
+            points: [
+                [30, 20],
+                [70, 80],
+            ],
+            fnType: 'points',
+            graphType: 'polyline',
+            color: 'red'
+        })
+        options.data.push({
+            points: [
+                [30, 80],
+                [70, 20],
+            ],
+            fnType: 'points',
+            graphType: 'polyline',
+            color: 'red'
+        })
+    } else {
+        options.data.push({
+            vector: second,
+            graphType: 'polyline',
+            fnType: 'vector',
+            skipTip: true,
+            color: secondColor,
+        })
+        options.data.push({
+            vector: first,
+            offset: second,
+            graphType: 'polyline',
+            fnType: 'vector',
+            skipTip: true,
+            color: firstColor,
+        })
+    }
+    vectors = functionPlot(options)
+}
+vectors.on("mousemove", vectormousemove)
